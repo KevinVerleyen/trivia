70c077d5dd00d3032ebdf70851e5a71f1a6eb67d
KevinVerleyen <verleyen-kevin@hotmail.be>
2025-04-13 20:35:54 +0200

    git

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonList,
+  IonItem,
+  IonSelect,
+  IonSelectOption,
+  IonLabel,
+  IonRange,
+  IonButton,
+  IonNote,
+} from '@ionic/angular/standalone';
+import { TriviaApiService } from 'src/app/services/trivia-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonNote,
+    IonItem,
+    IonList,
+    IonCard,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonSelect,
+    IonSelectOption,
+    IonLabel,
+    IonRange,
+    IonButton,
+  ],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  categories: { id: number; name: string }[] = []; // Structure des catégories
+numberOfQuestions: any;
+  constructor(private triviaService: TriviaApiService) {}
 
   ngOnInit() {
+    this.triviaService
+      .getCategories()
+      .subscribe((response: { trivia_categories: any }) => {
+        this.categories = response.trivia_categories; // Mise à jour des catégories
+      });
   }
-
 }
--- src/app/pages/quiz/quiz.page.ts
+++ src/app/pages/quiz/quiz.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCardTitle,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonItem,
+  IonRadio,
+  IonRadioGroup,
+  IonLabel,
+  IonList,
+  IonProgressBar,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.page.html',
   styleUrls: ['./quiz.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonProgressBar,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonItem,
+    IonRadioGroup,
+    IonLabel,
+    IonRadio,
+    IonList,
+    IonButton,
+  ],
 })
 export class QuizPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  [x: string]: any;
+  data: { [key: string]: any } = { questionIndex: 0 } ;
+  nextQuestion() {
+    this['questionIndex']++;
   }
+   constructor() {}
 
+  ngOnInit() {}
 }
